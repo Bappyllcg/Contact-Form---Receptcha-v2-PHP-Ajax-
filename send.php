5a49135b4911573366e6d3005eb857804107e70c
SA Bappy <31420819+Bappyllcg@users.noreply.github.com>
2022-11-15 00:47:05 +0600

    update send php

--- send.php
+++ send.php
@@ -2,17 +2,22 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "bappy.llcg@gmail.com";
+        $mail_to = "gabyjoemedia@gmail.com";
         
         # Sender Data
         $name = trim($_POST["name"]);
         $email = trim($_POST["email"]);
         $phone = trim($_POST["phone"]);
-        $format = trim($_POST["format"]);
+        $cetagory = trim($_POST["cetagory"]);
+        if(empty($cetagory)){
+            $cetagory = ' ';
+        }
         $message = trim($_POST["message"]);
-
-        
-        if ( empty($name) OR empty($email) OR empty($phone) OR empty($format) OR empty($message)) {
+        if(empty($message)){
+            $message = ' ';
+        }
+		
+        if (empty($name) OR empty($email) OR empty($phone)) {
             # Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Please complete the form and try again.";
@@ -20,15 +25,20 @@
         }
 
         // getting the captcha
-    $captcha = '';
-    if (isset($_POST['g-recaptcha-response']))
-        $captcha = $_POST['g-recaptcha-response'];
-    //echo 'captcha: '.$captcha;
+		$captcha = '';
+        if (isset($_POST['g-recaptcha-response'])){
+            $captcha = $_POST['g-recaptcha-response'];
+        }
 
-    if (!$captcha)
-        echo 'The captcha has not been checked.';
+        if (!$captcha){
+            # Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo 'Please check captcha.';
+            exit;
+        }
+        
         // handling the captcha and checking if it's ok
-        $secret = '6LeaO3MaAAAAAMBsfPA_1pP9VmogOnwlQdadl0Ip';
+        $secret = '6LcCUggjAAAAAEJZdKzXcQRv96TuOaQJuJf-FJ5D_';
         $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
 
         if ($response['success'] != false) {
@@ -40,30 +50,32 @@
         }
         
         # Mail Content
-        $content = "Name: $name\n";
-        $content .= "Email: $email\n";
-        $content .= "Phone: $phone\n";
-        $content .= "Format: $format\n";
-        $content .= "Message: $message\n";
+		$content = "Name: $name\n";
+		$content .= "Email: $email\n";
+		$content .= "Phone: $phone\n";
+		$content .= "Category: $cetagory\n";
+		$content .= "Message: $message\n";
 
-        # email headers.
-        $headers = "From: $name &lt;$email&gt;";
+		# email headers.
+		$headers = "From: $first_name &lt;$email&gt;";
 
-        # Send the email.
-        $success = mail($mail_to,'New Message From: '.$name.' ', $content);
-        if ($success && $response['success']) {
-            # Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            # Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong, we couldn't send your message.";
-        }
+		# Send the email.
+		$success = mail($mail_to,'New Message From: '.$name.'-'.$promotion, $content);
+		if ($success) {
+			# Set a 200 (okay) response code.
+			http_response_code(200);
+			echo "Thank You! Your message has been sent.";
+			exit;
+		} else {
+			# Set a 500 (internal server error) response code.
+			http_response_code(500);
+			echo "Oops! Something went wrong, we couldn't send your message.";
+			exit;
+		}
 
-        } else {
-            # Not a POST request, set a 403 (forbidden) response code.
-            http_response_code(403);
-            echo "There was a problem with your submission, please try again.";
-        }
+	} else {
+		# Not a POST request, set a 403 (forbidden) response code.
+		http_response_code(403);
+		echo "There was a problem with your submission, please try again.";
+	}
 ?>
